2ee3f8c68692dc50730db7247582ca4e0889a4ac
ashah28 <ashah28@users.noreply.github.com>
2017-06-19 10:56:23 -0700

    Periodic communication for better utilization

--- stripSensor.ino
+++ stripSensor.ino
@@ -2,17 +2,17 @@
 int stripSensor = 2;
 unsigned long lastUpdate = 0;
 int lastValue = 0;
-bool plot = false;
+unsigned long int timeElapsed = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(9600);
   pinMode(stripSensor, INPUT);
-  //lastUpdate = millis();
-  lastUpdate = micros();
+  timeElapsed = micros();
 }
 
 int last_delta;
+int stripsTravelledLastSec = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
@@ -20,30 +20,36 @@ void loop() {
   int delta;
   if(lastValue != stripSensorResult)
   {
+    stripsTravelledLastSec++;
     lastValue = stripSensorResult;
     delta = micros()/100 - lastUpdate;
     lastUpdate = micros()/100;
 
     //Added code to minimise false reportings if this delta is drastically different than the last
-    if(plot && abs(delta - last_delta) > last_delta * .25 )
-    {
-      last_delta = delta;
-      return;
-     }
+//    if(abs(delta - last_delta) > last_delta * .75 )
+//    {   
+//      Serial.print("===>");
+//      Serial.print(',');   
+//      Serial.print(delta);
+//      Serial.print(',');
+//      Serial.print(last_delta);
+//      Serial.print(',');
+//      Serial.println(delta - last_delta
+//      );
+//      last_delta = delta;
+//      //return;
+//     }
     last_delta = delta;
-
-
-    if(plot)
-    {
-      Serial.print(stripSensorResult);
-      Serial.print(",");
-      Serial.print(delta);
-      Serial.print(",");
-      Serial.println((float)delta/1000);
-    }
-    else
-    {
-      Serial.println(1);
-    }
   }
+  
+  if(micros() - timeElapsed >= 200000)
+  {      
+    Serial.println(stripsTravelledLastSec);
+//    Serial.print(',');
+//    Serial.println(micros() - timeElapsed);
+      stripsTravelledLastSec = 0;
+      timeElapsed = micros();
+  }
+//  else
+//    Serial.println("0");
 }
