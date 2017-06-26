1dd8c1cd1897de72ac282081264513baedeebc75
ashah28 <ashah28@users.noreply.github.com>
2017-06-26 15:55:41 -0700

    Using moving average for smoother values

--- stripSensor.ino
+++ stripSensor.ino
@@ -1,10 +1,17 @@
+#include <Filter.h>
+
+Moving_average ma2(5, 0); // change the value divider appropriately to avoid int overflow
+
+//Need a divide the speedRate to keep it as an int and avoid overflow
+int valueDivider = 2;
 
 int stripIRSensor = 2;
 int distPerStripe = 150/12;
-unsigned long refreshInTime = 300;
+//refresh time in miliseconds
+unsigned long refreshInTime = 100;
 float perTimeUnit = 1000.0/refreshInTime;
 
-float cmSecToKmHrMultiplier = 3600.0/(100.0 * 1000.0);
+float cmSecToMHrMultiplier = 3600.0/(100.0);
 
 int lastValueIR = 0;
 int stripsTravelledLastSecIR = 0;
@@ -14,8 +21,8 @@ unsigned long timeElapsed = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(stripIRSensor, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
+//  pinMode(3, INPUT);
+//  pinMode(4, INPUT);
   timeElapsed = millis();
 }
 
@@ -34,13 +41,10 @@ void loop() {
     int distanceTravelled = stripsTravelledLastSecIR * distPerStripe;
     //speed is a keyword
     //speedrate is cm travelled per sec
-    float speedRate = distanceTravelled * perTimeUnit;
-    String data = "[" + 
-//    String(stripsTravelledLastSecIR)
-    String(speedRate * cmSecToKmHrMultiplier ) + "]"
-//    + ", " +
-//    String(speedRate)
-    ;
+    float speedRate = distanceTravelled * perTimeUnit * cmSecToMHrMultiplier;
+    //Data is current speed in m/hr /(10*valueDivider)
+    int filter2 = ma2.filter(int(speedRate/(10 * valueDivider))) * valueDivider;
+    String data = "[" + String(filter2) + "]";
     Serial.println(data);
     stripsTravelledLastSecIR = 0;
     timeElapsed = millis();
