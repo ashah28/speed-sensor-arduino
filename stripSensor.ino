c9f4ce02cfa18de8d058d8bd14e1583280f2e613
ashah28 <ashah28@users.noreply.github.com>
2017-06-26 13:28:41 -0700

    Sending precise distance travelled for better control

--- stripSensor.ino
+++ stripSensor.ino
@@ -1,35 +1,27 @@
 
 int stripIRSensor = 2;
-int hallSensor = 3;
-int hall2Sensor = 4;
+int distPerStripe = 150/12;
+unsigned long refreshInTime = 300;
+float perTimeUnit = 1000.0/refreshInTime;
+
+float cmSecToKmHrMultiplier = 3600.0/(100.0 * 1000.0);
 
 int lastValueIR = 0;
-int lastValueHall = 0;
-int lastValueHall2 = 0;
-
 int stripsTravelledLastSecIR = 0;
-int stripsTravelledLastSecHall = 0;
-int stripsTravelledLastSecHall2 = 0;
-
-unsigned long int timeElapsed = 0;
+unsigned long timeElapsed = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(9600);
   pinMode(stripIRSensor, INPUT);
-  pinMode(hallSensor, INPUT);
-  pinMode(hall2Sensor, INPUT);
-  timeElapsed = micros();
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+  timeElapsed = millis();
 }
-
-//int last_delta;
-
 
 // the loop routine runs over and over again forever:
 void loop() {
   int stripIRSensorResult = digitalRead(stripIRSensor);
-  int stripHallSensorResult = digitalRead(hallSensor);
-  int stripHall2SensorResult = digitalRead(hall2Sensor);
   
   if(lastValueIR != stripIRSensorResult)
   {
@@ -37,32 +29,20 @@ void loop() {
     lastValueIR = stripIRSensorResult;
   }
 
-  if(lastValueHall != stripHallSensorResult)
-  {
-    stripsTravelledLastSecHall++;
-    lastValueHall = stripHallSensorResult;
-  }
-
-  if(lastValueHall2 != stripHall2SensorResult)
-  {
-    stripsTravelledLastSecHall2++;
-    lastValueHall2 = stripHall2SensorResult;
-  }  
-  
-  if(micros() - timeElapsed >= 10000)
+  if(millis() - timeElapsed >= refreshInTime)
   { 
-    String data = String(stripsTravelledLastSecIR) + ","+ String(stripsTravelledLastSecHall + 5) 
-    + "," + String(stripsTravelledLastSecHall2 + 10) 
-    +  "," + String(stripsTravelledLastSecHall - stripsTravelledLastSecHall2)
+    int distanceTravelled = stripsTravelledLastSecIR * distPerStripe;
+    //speed is a keyword
+    //speedrate is cm travelled per sec
+    float speedRate = distanceTravelled * perTimeUnit;
+    String data = "[" + 
+//    String(stripsTravelledLastSecIR)
+    String(speedRate * cmSecToKmHrMultiplier ) + "]"
+//    + ", " +
+//    String(speedRate)
     ;
     Serial.println(data);
     stripsTravelledLastSecIR = 0;
-    stripsTravelledLastSecHall = 0;
-    stripsTravelledLastSecHall2 = 0;
-    timeElapsed = micros();
+    timeElapsed = millis();
   }
-  
-//  unsigned long int b = micros();
-//  String data = String(b-a) ;//+ "   " +String(a)+ "   " +String(b);
-//  Serial.println(data);
 }
